771b273d9edd1afbdc41bc2f2c53b20c31866a88
Sergey <spushkovskiy@gmail.com>
2023-04-23 21:00:24 +0300

    Added more meta tags

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,11 +10,38 @@ const Layout: FC<Children> = ({ children }) => {
   return (
     <>
       <DefaultSeo
-        title={'WisExpert'}
-        description={'Amazing WisExpert company'}
-        additionalLinkTags={[{ rel: 'icon', href: '/favicon.ico' }]}
+        title={'WisExpert | Бухгалтерська компанія'}
+        description={
+          'Якісні бухгалтерські послуги для ІТ компаній, ресторанів, виробництва, торгівлі, будівництва та ін. Дізнайтесь ціни та умови на сайті'
+        }
+        openGraph={{
+          siteName: 'WisExpert',
+          type: 'website',
+          title: 'Бухгалтерська компанія WisExpert',
+          url: 'wisexpert.com.ua/',
+          locale: 'ua_UA',
+          description:
+            'Якісні бухгалтерські послуги для ІТ компаній, ресторанів, виробництва, торгівлі, будівництва та ін. Дізнайтесь ціни та умови на сайті',
+          images: [
+            {
+              url: 'wisexpert.com.ua/favicon.png',
+              width: 600,
+              height: 600,
+              alt: 'Og image',
+            },
+          ],
+        }}
+        additionalLinkTags={[
+          { rel: 'shortcut icon', href: '/favicon.ico', type: 'image/png' },
+          { rel: 'icon', href: '/favicon.ico' },
+        ]}
         additionalMetaTags={[
           { name: 'Viewport', content: 'width=device-width, initial-scale=1' },
+          {
+            name: 'keywords',
+            content:
+              'бухгалтер, налог, отчет, бухгалтерія, звіт, податок, учет, ФОП, ТОВ, ФЛП, ООО, консультація, РРО, ЕСВ, ЄСВ, Бухгалтерская компания, Бухгальтерська фірма, компанія, послуги бухгалтера, услуги бухгалтера',
+          },
         ]}
       />
       <Header />
