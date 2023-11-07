5756d95689dfdc9dbc4a7616ff567fb3a0872537
Sergey <spushkovskiy@gmail.com>
2023-11-07 22:18:04 +0200

    Changed email

--- constants/contact.const.ts
+++ constants/contact.const.ts
@@ -1,6 +1,6 @@
 export const CONTACTS = {
-  email: 'mailto:info@wisexpert.com.ua',
-  emailDisp: 'info@wisexpert.com.ua',
+  email: 'mailto:w.expert.company@gmail.com',
+  emailDisp: 'w.expert.company@gmail.com',
   phone: 'tel:+380993811118',
   phoneDisp: '+38(099)381-11-18',
 };
