a8385be85f2ef9674c693660c71ec35c113b7c01
Sergey <spushkovskiy@gmail.com>
2024-04-12 17:33:06 +0300

    Updated Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,23 +3,25 @@ import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
 import { ContextProvider } from '@/context/GlobalContext';
 import Script from 'next/script';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <Layout>
       <ContextProvider>
-        <Script
+        {/* <Script
           strategy="lazyOnload"
           src="https://www.googletagmanager.com/gtag/js?id=AW-11421126867"
         />
         <Script id="google-analytics">
           {`
-          window.dataLayer = window.dataLayer || []; 
-          function gtag(){dataLayer.push(arguments);} 
-          gtag('js', new Date()); 
-          gtag('config', 'AW-11421126867'); 
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-11421126867');        
         `}
-        </Script>
+        </Script> */}
+        <GoogleAnalytics gaId="AW-11421126867" />
         <Script id="notion">
           {`
             try {
