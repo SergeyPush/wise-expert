1b5ac7b9e102dd13c05f2282a8a4d15413a19f64
Sergey Pushkovskiy <spushkovskiy@gmail.com>
2025-10-16 18:01:39 +0300

    Improved SEO features

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,33 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="uk">
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'Organization',
+              name: 'WisExpert',
+              url: 'https://wisexpert.com.ua',
+              logo: 'https://wisexpert.com.ua/favicon.png',
+              description:
+                'Якісні бухгалтерські послуги для ІТ компаній, ресторанів, виробництва, торгівлі, будівництва',
+              address: {
+                '@type': 'PostalAddress',
+                addressCountry: 'UA',
+                addressLocality: 'Ukraine',
+              },
+              contactPoint: {
+                '@type': 'ContactPoint',
+                contactType: 'customer service',
+                availableLanguage: ['Ukrainian', 'Russian'],
+              },
+            }),
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
